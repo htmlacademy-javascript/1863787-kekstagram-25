d1ba7d43b1e1b17232b7f9bec292c40faf11d448
Andrey <andreiesin96159@gmail.com>
2022-02-28 18:43:30 +0200

    fix js

--- js/main.js
+++ js/main.js
@@ -1,29 +1,17 @@
 function randomNumber(min, max){
   const r = Math.random()*(max-min);
-  if(Math.floor(r) == 0){
-  console.log('это 0');
-  }else{
-    console.log("это не 0");
-  }
-  if(max > min){
-    console.log('min меньшу max');
-  }else{
-    console.log('max меньшу min');
-  }
-  console.log(Math.floor(r));
-  return Math.floor(r);
+  let result = (Math.floor(r) === 0) ?  console.log('это 0') :
+  (max > min) ? console.log('min меньшу max') :
+  (max < min) ? console.log('max меньшу min') : console.log('что то другое');
 }
 
 randomNumber(0, 10);
 
 
+
+
 function longLine(string_check, maximum_length){
-for(let i = 0; i < string_check.length; i++){
-  if(string_check[i].length == maximum_length){
-    console.log("true")
-  }else{
-    console.log("false")
-  }
+ return (string_check.length === maximum_length);
 }
-}
-longLine(["Hi"], [2]);
+console.log(longLine("Hi", 2));
+
