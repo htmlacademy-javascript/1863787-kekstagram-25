ee8d170619446d6c0d0eb45a013cb67659317e5b
Keks <keks@htmlacademy.ru>
2022-04-02 08:36:41 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/data.js
@@ -0,0 +1,55 @@
+export const photoObj = [
+  { id: 0, url: '1' },
+  { id: 1, url: '2' },
+  { id: 2, url: '3' },
+  { id: 3, url: '4' },
+  { id: 4, url: '5' },
+  { id: 5, url: '6' },
+  { id: 6, url: '7' },
+  { id: 7, url: '8' },
+  { id: 8, url: '9' },
+  { id: 9, url: '10' },
+  { id: 10, url: '11' },
+  { id: 11, url: '12' },
+  { id: 12, url: '13' },
+  { id: 13, url: '14' },
+  { id: 14, url: '15' },
+  { id: 15, url: '16' },
+  { id: 16, url: '17' },
+  { id: 17, url: '18' },
+  { id: 18, url: '19' },
+  { id: 19, url: '20' },
+  { id: 20, url: '21' },
+  { id: 21, url: '22' },
+  { id: 22, url: '23' },
+  { id: 23, url: '24' },
+  { id: 24, url: '25' }
+];
+export const description = [
+  { id: 0, opis: 'первое' },
+  { id: 1, opis: 'второе' },
+  { id: 2, opis: 'третье' },
+];
+
+export const nameFolov = [{ name: 'Vasi' }, { name: 'Peti' }, { name: 'Andre' }];
+export const maximum = 6;
+export const text = ['Всё отлично!', 'В целом всё неплохо. Но не всё.'];
+export const arrText = Math.floor(Math.random() * text.length);
+
+export const pictures = document.querySelector('.pictures');
+export const template = document.querySelector('#picture').content;
+export const bigPicture = document.querySelector('.big-picture');
+export const bigPictureS = document.querySelector('.big-picture__img');
+export const cancelButton = document.querySelector('.big-picture__cancel.cancel');
+export const commentsCount = document.querySelector('.comments-count');
+export const body = document.querySelector('body');
+export const socialCaption = document.querySelector('.social__caption');
+export const socialComment = document.querySelector('.social__comments');
+export const avatar = [
+  { id: 0, url: 'avatar-1' },
+  { id: 1, url: 'avatar-2' },
+  { id: 2, url: 'avatar-3' },
+  { id: 3, url: 'avatar-4' },
+  { id: 4, url: 'avatar-5' },
+  { id: 5, url: 'avatar-6' }
+];
---
+++ 6/js/util.js
@@ -0,0 +1,139 @@
+import { photoObj, nameFolov, maximum, text, arrText, pictures, template, bigPicture, bigPictureS, cancelButton, commentsCount, body, socialCaption, description, socialComment, avatar } from './data.js';
+
+//4.9. Больше деталей
+export default function test() {
+
+  function randomLikes(max, min) {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+  randomLikes(200, 15);
+
+  function comment() {
+    nameFolov.map((nam, index) => ({
+      comments: {
+        id: index,
+        avatar: `img/avatar-${Math.floor(Math.random() * maximum)}.svg`,
+        message: `${text[arrText]}`,
+        name: nam,
+      }
+    }
+    ));
+  }
+  comment();
+
+
+  //ДЗ 7.1
+  //   const massiv = Object.keys(photoObj);
+  //  console.log(massiv);
+
+
+  photoObj.forEach((i) => {
+    const clonePopup = template.querySelector('.picture').cloneNode(true);
+    const pictureImg = clonePopup.querySelector('.picture__img');
+    pictureImg.src = `photos/${i.url}.jpg`;
+    pictureImg.setAttribute('data-id', `${i.id}`);
+
+    clonePopup.querySelector('.picture__likes').textContent = randomLikes(15, 200);
+    clonePopup.querySelector('.picture__comments').textContent = text.length;
+    pictures.append(clonePopup);
+  });
+
+  const bigPict = bigPictureS.querySelector('img');
+  const likesCount = document.querySelector('.likes-count');
+
+  //ДЗ 7.2
+
+
+  const pictureLikes = document.querySelectorAll('.picture__likes');
+  pictures.addEventListener('click', (evt) => {
+    bigPicture.classList.remove('hidden');
+    body.classList.add('modal-open');
+
+    document.querySelector('.social__comment-count').classList.add('hidden'); // временная hidden
+    document.querySelector('.comments-loader').classList.add('hidden');// временная hidden
+
+    bigPict.src = evt.target.src;
+
+    const id = Number(evt.target.dataset.id);
+    likesCount.innerHTML = pictureLikes[id].innerHTML;
+
+    commentsCount.innerHTML = text.length;
+
+    description.forEach((i) => {
+      socialCaption.innerHTML = i.opis;
+    });
+
+  });
+
+
+  cancelButton.onclick = function () {
+    bigPicture.classList.add('hidden');
+    body.classList.remove('modal-open');
+  };
+
+  document.addEventListener ('keydown', (evt)=>{
+    if (evt.key === 'Escape'){
+      evt.preventDefault();
+      bigPicture.classList.add('hidden');
+      body.classList.remove('modal-open');
+    }
+  })
+
+}
+test();
+
+avatar.forEach((ava) => {
+  const social = socialComment.querySelector('.social__comment').cloneNode(true);
+  const socialText = social.querySelector('.social__text');
+  const socialPicture = social.querySelector('.social__picture');
+  socialPicture.src = `img/${ava.url}.svg`;
+
+  nameFolov.forEach((nam) => {
+    socialPicture.alt = nam.name;
+  });
+  text.forEach((tex) => {
+    socialText.innerHTML = tex;
+  });
+
+  socialComment.append(social);
+
+});
+socialComment.removeChild(socialComment.children[0]);
+socialComment.removeChild(socialComment.children[0]);
+
+
+// ДЗ 2
+// function randomNumber(min, max) {
+//   const r = Math.random() * (max - min);
+//   (Math.floor(r) === 0) ? console.log('это 0') :
+//     (max > min) ? console.log('min меньшу max') :
+//       (max < min) ? console.log('max меньшу min') : console.log('что то другое');
+// }
+
+// randomNumber(0, 10);
+
+// function longLine(string_check, maximum_length) {
+//   return (string_check.length === maximum_length);
+// }
+// longLine("Hi", 2);
+
+// function getRandomPositiveInteger(a, b) {
+
+//   const array = a.concat(b);
+//   let minArray = Math.min.apply(null, array);
+//   let maxArray = Math.max.apply(null, array);
+
+//   const lower = Math.ceil(Math.min(Math.abs(minArray), Math.abs(maxArray)));
+//   const upper = Math.floor(Math.max(Math.abs(minArray), Math.abs(maxArray)));
+
+//   const result = Math.random() * (upper - lower + 1) + lower;
+
+//   return Math.floor(result);
+// }
+
+// getRandomPositiveInteger([1.2, -0.5, 4.6,], [6.6, 2.1, -3.3]);
+
+// function checkStringLength(string, length) {
+//   return string.length <= length;
+// }
+// checkStringLength('var', 'bard');
